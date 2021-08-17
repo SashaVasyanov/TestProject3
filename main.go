8e3883e5bf5202c47eab0ef33be1fe69b9e5d793
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-08-17 21:43:33 +0300

    Calculator

--- main.go
+++ main.go
@@ -12,4 +12,15 @@ func main() {
 	fmt.Println(true || false)
 	fmt.Println(!true)
 	fmt.Println(false && false)
+	var number int64
+	fmt.Scanf("%d\n", &number)
+	var number2 int64
+	var number3 int64
+	var number4 int64
+	fmt.Scanf("%d\n", &number2)
+	fmt.Scanf("%d\n", &number3)
+	fmt.Scanf("%d\n", &number4)
+	fmt.Println(number + number2)
+	fmt.Println(number3 * number4)
+	fmt.Println(number / number2)
 }
