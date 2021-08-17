ae33fe8eb676584223e4f42b07b43c111eb0d5c6
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-08-17 21:09:37 +0300

    Add new functions

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("2 + 1 =", 1+(1+1))
+	fmt.Println(len("Я люблю аниме и не думаю о том, что другие люди считаю по этому поводу"))
+	fmt.Println("Sалава плюс поплава"[0])
+	fmt.Println("ТвояМать" + "Шлюха")
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(false && false)
 }
